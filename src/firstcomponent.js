aead977fdd0757da20c2f2ae2f6c5920add34be9
RANJAN Aditya <aditya.ranjan@soprateria.com>
2021-08-10 10:03:17 +0530

    first react component

---
+++ src/firstcomponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+function firstcomponent(){
+    return(
+        <React.Fragment>
+            <div>
+                <h2>
+                    This is my first react component.
+                </h2>
+            </div>
+        </React.Fragment>
+    )
+}
+export default firstcomponent
